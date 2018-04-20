ffadbd8718f681913a1ba926ae91d380b9edd60d
thinhle93 <thinhle93@gmail.com>
2018-04-19 22:52:46 -0700

    changes made for beltreviewer

--- Gemfile
+++ Gemfile
@@ -44,6 +44,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'hirb'
-  gem 'rails-footnotes', '>= 4.0.0', '<5'
+  #gem 'rails-footnotes', '>= 4.0.0', '<5'
 end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,15 @@ class Event < ActiveRecord::Base
   has_many :attends, dependent: :delete_all
   has_many :comments, dependent: :delete_all
   has_many :attendees, through: :attends, source: :user
-  validates :name, :city, :state, :date, presence: true
+  validates :name, :city, :state, presence: true
   validate :future_event
 
   private
     def future_event
-      errors.add(:date, "can't be in the past!") if date < Time.now
+      if date == nil
+        errors.add(:date, "can't be empty")
+      elsif date < Time.now
+        errors.add(:date, "can't be in the past!")
+      end
     end
 end
